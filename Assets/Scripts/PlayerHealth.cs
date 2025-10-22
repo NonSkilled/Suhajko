15e4e83c142067445c26d617c38d67c0e5214a7b
NonSkilled <d.olearnik@ostrovskeho.com>
2025-10-22 08:03:43 +0200

    Update

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,11 +6,13 @@ public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float maxHealth = 3;
+    public float interval = 3f;
     private void Start()
     {
         health = maxHealth;
-        
     }
+    
+    
 
    
 
